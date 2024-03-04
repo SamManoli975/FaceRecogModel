f4f57c5a30485f67e2bd88a6e192b9d087921107
SamManoli975 <C23330671@mytudublin.ie>
2024-03-04 21:57:53 +0000

    commenting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 
@@ -9,6 +10,7 @@ cap = cv2.VideoCapture(0)
 
 mp_face_mesh = mp.solutions.face_mesh
 
+#setting the max faces and confidence levels
 with mp_face_mesh.FaceMesh(
         max_num_faces = 1,
         refine_landmarks = True,
@@ -16,7 +18,7 @@ with mp_face_mesh.FaceMesh(
         min_tracking_confidence = 0.5
     ) as face_mesh:
     
-
+    #while camera is opened read camera
     while cap.isOpened():
         success, image = cap.read()
         if not success:
@@ -24,12 +26,16 @@ with mp_face_mesh.FaceMesh(
             
             
         results = face_mesh.process(image)
+
+        #if there are landmarks in sight
         if results.multi_face_landmarks:
+            #for each coordinate in the data
             for face_landmarks in results.multi_face_landmarks:
+                #draw the landmarks
                 mp_drawing.draw_landmarks(
                     image = image,
                     landmark_list = face_landmarks,
-                    connections = mp_face_mesh.FACEMESH_TESSELATION,
+                    connections = mp_face_mesh.FACEMESH_TESSELATION,#connect the landmarks
                     landmark_drawing_spec = None,
                     connection_drawing_spec = mp_drawing_styles
                     .get_default_face_mesh_tesselation_style()
@@ -44,9 +50,10 @@ with mp_face_mesh.FaceMesh(
     #                 .get_default_face_mesh_contours_style()
                 )
 
-
+        #show the camera
         cv2.imshow("My video capture", cv2.flip(image, 1))
 
+        #if q is pressed end the camera and destroy the windows
         if cv2.waitKey(100) & 0xFF == ord('q'):
             break
         
