0e8511858b244c74cd2c225638042fc2b9b75127
SamManoli975 <C23330671@mytudublin.ie>
2024-03-11 17:58:10 +0000

    few changes

--- main.py
+++ main.py
@@ -34,9 +34,9 @@ def calc_landmark_list(image, landmarks):
     for _, landmark in enumerate(landmarks.landmark):
         landmark_x = min(int(landmark.x * image_width), image_width - 1)
         landmark_y = min(int(landmark.y * image_height), image_height - 1)
-        # landmark_z = landmark.z
 
-        landmark_point.append([landmark_x, landmark_y])
+        landmark_point.append(int(landmark_x))
+        landmark_point.append(int(landmark_y))
 
     return landmark_point
 
@@ -47,21 +47,28 @@ def pre_process_landmark(landmark_list):
     # Convert to relative coordinates
     base_x, base_y = 0, 0
     for index, landmark_point in enumerate(temp_landmark_list):
-        if index == 0:
-            base_x, base_y = landmark_point[0], landmark_point[1]
+        if isinstance(landmark_point, (list, tuple)):
+            if index == 0:
+                base_x, base_y = landmark_point[0], landmark_point[1]
 
-        temp_landmark_list[index][0] = temp_landmark_list[index][0] - base_x
-        temp_landmark_list[index][1] = temp_landmark_list[index][1] - base_y
+            temp_landmark_list[index][0] = temp_landmark_list[index][0] - base_x
+            temp_landmark_list[index][1] = temp_landmark_list[index][1] - base_y
 
-    # Convert to a one-dimensional list
-    temp_landmark_list = list(
-        itertools.chain.from_iterable(temp_landmark_list))
+    # Flatten the list of lists or tuples
+    temp_landmark_list_flat = []
+    for point in temp_landmark_list:
+        if isinstance(point, (list, tuple)):
+            temp_landmark_list_flat.extend(point)
+        else:
+            temp_landmark_list_flat.append(point)
 
     # Normalization
-    max_value = max(list(map(abs, temp_landmark_list)))
+    max_value = max(map(abs, temp_landmark_list_flat))
 
     def normalize_(n):
         return n / max_value
+
+    return temp_landmark_list_flat
 
     
 
@@ -89,7 +96,7 @@ with mp_face_mesh.FaceMesh(
                 # print(face_landmarks.landmark)
                 #print the coorinates every landmarks
                 landmark_list = calc_landmark_list(image, face_landmarks)
-                # landmark_list = pre_process_landmark(landmark_list)
+                landmark_list = pre_process_landmark(landmark_list)
                
                 writer.writerow(landmark_list)
 
